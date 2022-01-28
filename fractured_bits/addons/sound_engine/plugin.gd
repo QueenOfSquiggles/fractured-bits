5978c94bc7de916662cf1c424fa500ab68e94c0b
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2022-01-28 13:47:27 -0600

    Ported TimeSlice to an addon, started work on "sound_engine"
    Sound engine will have more functionality, but for now it is just the sound library system from PigDev's sound tutorial

--- fractured_bits/addons/sound_engine/plugin.gd
+++ fractured_bits/addons/sound_engine/plugin.gd
@@ -3,8 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	pass
+	var script := preload("res://addons/sound_engine/sound_lib/SoundLib.gd")
+	add_custom_type("SoundLib", "Node", script, preload("res://addons/sound_engine/icons/sound_lib.svg"))
+	add_custom_type("SoundLib2D", "Node2D", script, preload("res://addons/sound_engine/icons/sound_lib_2D.svg"))
+	add_custom_type("SoundLib3D", "Spatial", script, preload("res://addons/sound_engine/icons/sound_lib_3D.svg"))
 
 
 func _exit_tree() -> void:
-	pass
+	remove_custom_type("SoundLib")
+	remove_custom_type("SoundLib2D")
+	remove_custom_type("SoundLib3D")
