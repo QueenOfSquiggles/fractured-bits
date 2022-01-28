c3a4809d7f5d07db00f558ef244ec81badda08ea
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2022-01-28 13:11:30 -0600

    Adding modules from MiniNautilus
    I made several modules when working on MiniNautilus, they still need to be fixed up before they will work as proper addons so they will stay in the module category for now

---
+++ fractured_bits/modules/item_system/Items.gd
@@ -0,0 +1,18 @@
+extends Node
+"""
+An Autoload script for handling item system registration. Essentially a conversion utility between item resource references and item ids
+"""
+
+export (Array, Resource) var registered_items := []
+
+func get_item(id : int) -> Item:
+	if id >= 0 and id < registered_items.size():
+		return registered_items[id]
+	return null
+
+func get_id(item : Item) -> int:
+	for i in range(registered_items.size()):
+		if item == registered_items[i]:
+			return i
+	return 0
+
